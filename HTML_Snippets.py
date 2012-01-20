81a74de786e01c77610343ede0fdec2ab2ce67c1
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2012-01-20 13:38:37 +0000

    Added Google Analytics tracker

--- HTML_Snippets.py
+++ HTML_Snippets.py
@@ -147,6 +147,24 @@ background-color: black;
 }
 
 </style>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28507011-1']);
+  _gaq.push(['_setDomainName', 'zaqwsxedce.appspot.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+
 </head>
 <body> """ % (page_title, id_tab, breadcrumbs)
 
